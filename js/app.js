b1f3f13fec618175596ad68fcb5f7d198929ac28
xavierchia <xavier.cpy@gmail.com>
2020-07-09 13:28:36 +0800

    Organized files, added comments, changed images, added grid

---
+++ js/app.js
@@ -0,0 +1,60 @@
+
+// Creating the image array
+var imgArray = [];
+imgArray[0] = new Image();
+imgArray[0].src = "assets/portrait0.png";
+imgArray[1] = new Image();
+imgArray[1].src = "assets/portrait1.png";
+imgArray[2] = new Image();
+imgArray[2].src = "assets/portrait2.png";
+imgArray[3] = new Image();
+imgArray[3].src = "assets/portrait3.png";
+
+// Overlays current drawing to the right image and reduces opacity
+function doneDrawing() {
+    canImage.context.drawImage(imgArray[imgNumber], 0, 0)
+    canImage.changeOpacity(0.3);
+    canDraw.overlay(canOverlay);
+    canOverlay.changeOpacity(0.5);
+}
+
+
+// Cycles through images in the library 
+var imgNumber = 0;
+function nextImage() {
+    imgNumber++;
+    imgNumber = imgNumber % (imgArray.length);
+    canDraw.erase();
+    canImage.erase();
+    canOverlay.erase();
+    canCorrect.erase();
+    if (!canImage.imageVisible) {
+        canImage.toggleImage();
+    }
+    canImage.context.drawImage(imgArray[imgNumber], 50, 50)
+}
+
+
+// Creating the four canvases 
+
+// Canvas for drawing on the left
+var canDraw = new Canvas("canDraw", "black");
+document.body.appendChild(canDraw.canvas);
+
+// Canvas to draw the image on the right
+var canImage = new Canvas("canImage", "black");
+document.body.appendChild(canImage.canvas);
+
+// Canvas to overlay the left drawing on the right image
+var canOverlay = new Canvas("canOverlay", "black");
+document.body.appendChild(canOverlay.canvas);
+
+// Canvas to do corrections on the right overlay
+var canCorrect = new Canvas("canCorrect", "red");
+document.body.appendChild(canCorrect.canvas);
+
+window.onload = function () {
+    canImage.context.drawImage(imgArray[0], 50, 50)
+}
+
+
---
+++ js/canvas.js
@@ -0,0 +1,118 @@
+
+// Canvas constructor
+var Canvas = function (canvasID, color = "black") {
+  this.canvas = document.createElement("canvas");
+  this.canvas.id = canvasID;
+  this.canvas.width = 400;
+  this.canvas.height = 400;
+  this.canvas.style.border = "1px solid";
+  this.canvas.style.position = "absolute";
+  this.context = this.canvas.getContext("2d");
+  this.prevX = 0;
+  this.prevY = 0;
+  this.currX = 0;
+  this.currY = 0;
+  this.isMousePressed = false;
+  this.penColor = color;
+  this.penSize = 3;
+  this.imageVisible = true;
+  this.backupImage = false;
+
+  // Event listeners only work for desktop mouse at the moment
+  this.canvas.addEventListener("mousemove", function (e) {
+    this.findxy("move", e)
+  }.bind(this), false);
+  this.canvas.addEventListener("mousedown", function (e) {
+    this.findxy("down", e)
+  }.bind(this), false);
+  this.canvas.addEventListener("mouseup", function (e) {
+    this.findxy("up", e)
+  }.bind(this), false);
+  this.canvas.addEventListener("mouseout", function (e) {
+    this.findxy("out", e)
+  }.bind(this), false);
+
+  // Drawing when the mouse is being moved after pressed
+  this.draw = function () {
+    this.context.beginPath();
+    this.context.lineJoin = "round";
+    this.context.lineCap = "round";
+    this.context.moveTo(this.prevX, this.prevY);
+    this.context.lineTo(this.currX, this.currY);
+    this.context.strokeStyle = this.penColor;
+    this.context.lineWidth = this.penSize;
+    this.context.stroke();
+    this.context.closePath();
+  };
+
+  this.erase = function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  };
+
+  // Copies the drawing from this canvas to the target canvas
+  this.overlay = function (targetCanvas) {
+    targetCanvas.context.drawImage(this.canvas, 0, 0);
+  };
+
+  // What should happen when the mouse events are different
+  this.findxy = function (res, e) {
+
+    // Create a rectangle when the mouse is pressed
+    if (res == 'down' && e.button === 0) {
+      this.prevX = this.currX;
+      this.prevY = this.currY;
+      this.currX = e.clientX - this.canvas.offsetLeft;
+      this.currY = e.clientY - this.canvas.offsetTop;
+
+      this.isMousePressed = true;
+      this.context.beginPath();
+      this.context.fillStyle = this.penColor;
+      this.context.fillRect(this.currX, this.currY, this.penSize, this.penSize);
+      this.context.closePath();
+    }
+
+    // Mouse is no longer pressed or cursor has left canvas boundary
+    if (res == 'up' || res == "out") {
+      this.isMousePressed = false;
+    }
+
+    // It should draw if mouse is pressed and mouse is moving
+    if (res == 'move') {
+      if (this.isMousePressed) {
+        this.prevX = this.currX;
+        this.prevY = this.currY;
+        this.currX = e.clientX - this.canvas.offsetLeft;
+        this.currY = e.clientY - this.canvas.offsetTop;
+        this.draw();
+      }
+    }
+  };
+
+  this.toggleImage = function () {
+    if (this.imageVisible) {
+      this.backupImage = this.backup();
+      this.erase();
+      this.imageVisible = false;
+    }
+    else {
+      this.context.drawImage(this.backupImage.canvas, 0, 0);
+      this.imageVisible = true;
+    }
+  };
+
+  // Backup image if you want to make changes
+  this.backup = function () {
+    var canBack = new Canvas('canBack');
+    canBack.context.drawImage(this.canvas, 0, 0);
+    return canBack;
+  }
+
+  this.changeOpacity = function (number) {
+    var canBack = this.backup();
+    this.context.save();
+    this.erase();
+    this.context.globalAlpha = number;
+    this.context.drawImage(canBack.canvas, 0, 0);
+    this.context.restore();
+  };
+}
